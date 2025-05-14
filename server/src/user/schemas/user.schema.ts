c5d6feacde5b1cab6c8a9bc586d48c717b12d945
Natalia Eremina <entalia663@gmail.com>
2025-05-15 00:11:04 +0200

    fix Document typing issues

--- server/src/user/schemas/user.schema.ts
+++ server/src/user/schemas/user.schema.ts
@@ -1,13 +1,26 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { Document, Types } from 'mongoose';
+import { ApiProperty } from '@nestjs/swagger';
 
-@Schema()
-export class User extends Document {
-  @Prop({ required: true, unique: true })
+@Schema({ timestamps: true })
+export class User {
+  @ApiProperty({ type: String })
+  _id: Types.ObjectId;
+
+  @ApiProperty()
+  @Prop({ required: true, unique: true, index: true })
   email: string;
 
   @Prop({ required: true })
   password: string;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
 }
+
+export type UserDocument = User & Document;
 
 export const UserSchema = SchemaFactory.createForClass(User);
