13b50f315e0e94b46b100afb91cbed26c9722364
Natalia Eremina <entalia663@gmail.com>
2025-05-15 00:10:33 +0200

    configure dependencies

--- server/src/transaction/transaction.module.ts
+++ server/src/transaction/transaction.module.ts
@@ -3,16 +3,17 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { TransactionService } from './transaction.service';
 import { TransactionController } from './transaction.controller';
 import { Transaction, TransactionSchema } from './schemas/transaction.schema';
-import { Account, AccountSchema } from '../account/schemas/account.schema';
+import { AccountModule } from '../account/account.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Transaction.name, schema: TransactionSchema },
-      { name: Account.name, schema: AccountSchema },
     ]),
+    AccountModule,
   ],
   providers: [TransactionService],
   controllers: [TransactionController],
+  exports: [TransactionService],
 })
 export class TransactionModule {}
