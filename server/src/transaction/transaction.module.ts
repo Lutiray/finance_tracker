17b89d3224a154dc16125e7842e983e3d3e7d2b4
Natalia Eremina <entalia663@gmail.com>
2025-05-14 15:36:27 +0200

    register mongoose models and services

---
+++ server/src/transaction/transaction.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TransactionService } from './transaction.service';
+import { TransactionController } from './transaction.controller';
+import { Transaction, TransactionSchema } from './schemas/transaction.schema';
+import { Account, AccountSchema } from '../account/schemas/account.schema';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Transaction.name, schema: TransactionSchema },
+      { name: Account.name, schema: AccountSchema },
+    ]),
+  ],
+  providers: [TransactionService],
+  controllers: [TransactionController],
+})
+export class TransactionModule {}
