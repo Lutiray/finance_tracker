de14adc95f57a3360e4b0164ba9c3c7d448c1e66
Natalia Eremina <entalia663@gmail.com>
2025-05-15 22:38:58 +0200

    - Update account schema - Add currency enum validation - Set RUB as default currency - Add timestamps

--- server/src/account/schemas/account.schema.ts
+++ server/src/account/schemas/account.schema.ts
@@ -1,16 +1,53 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
+import { ApiProperty } from '@nestjs/swagger';
+import { Currency, CurrencyValues } from '../../../common/enums/currency.enum';
 
-@Schema()
-export class Account extends Document {
-  @Prop({ required: true })
+
+export type AccountDocument = Account & Document;
+
+@Schema({ 
+  timestamps: true,
+  toJSON: {
+    virtuals: true,
+    transform: (doc, ret) => {
+      delete ret._id;
+      delete ret.__v;
+      delete ret.userId;
+    }
+  }
+})
+export class Account {
+  @ApiProperty({ type: String })
+  _id: Types.ObjectId;
+
+  @ApiProperty()
+  @Prop({ required: true, trim: true })
   name: string;
 
-  @Prop({ default: 0 })
+  @ApiProperty()
+  @Prop({ required: true, default: 0, min: 0 })
   balance: number;
 
-  @Prop({ required: true })
+  @ApiProperty({ enum: CurrencyValues})
+    @Prop({
+    type: String,
+    enum: CurrencyValues,
+    default: Currency.RUB,
+    required: true
+  })
+  currency: Currency;
+
+  @ApiProperty({ type: String })
+  @Prop({ type: Types.ObjectId, ref: 'User', required: true })
   userId: Types.ObjectId;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
 }
 
 export const AccountSchema = SchemaFactory.createForClass(Account);
+AccountSchema.index({ userId: 1, name: 1 }, { unique: true });
