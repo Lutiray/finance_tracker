6e0a5a0a935a920287b43ffb91da88b79dd04f18
Natalia Eremina <entalia663@gmail.com>
2025-05-17 19:40:27 +0200

    cleanup service after auth changes

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable, NotFoundException, ConflictException } from '@nestjs/common
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from './schemas/user.schema';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
@@ -19,12 +19,12 @@ export class UserService {
     return user;
   }
 
-  async create(email: string, password: string): Promise<UserDocument> {
-    const existingUser = await this.findByEmail(email);
-    if (existingUser) {
+  async create(email: string, hashedPassword: string): Promise<UserDocument> {
+
+    if (await this.userModel.exists({ email })) {
       throw new ConflictException('Email already in use');
     }
-    const hashedPassword = await bcrypt.hash(password, 12);
+
     return this.userModel.create({ email, password: hashedPassword });
   }
 
