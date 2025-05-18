6b80d2894ae9e73c74c7e9856ca20110759136b7
Natalia Eremina <entalia663@gmail.com>
2025-05-18 22:20:00 +0200

    ensure proper ObjectId conversion in findById

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, NotFoundException, ConflictException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 import { User, UserDocument } from './schemas/user.schema';
 import * as bcrypt from 'bcryptjs';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -14,7 +14,10 @@ export class UserService {
   }
 
   async findById(id: string): Promise<UserDocument | null> {
-    const user = await this.userModel.findById(id).select('-password').exec();
+
+    const user = await this.userModel.findById(new Types.ObjectId(id))
+      .select('-password').exec();
+
     if (!user) throw new NotFoundException('User not found');
     return user;
   }
