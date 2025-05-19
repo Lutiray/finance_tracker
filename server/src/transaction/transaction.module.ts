0ebeb6d56c992ded6d930f0da3bc6d9c7dc74984
Natalia Eremina <entalia663@gmail.com>
2025-05-19 21:50:14 +0200

    add category module

--- server/src/transaction/transaction.module.ts
+++ server/src/transaction/transaction.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TransactionService } from './transaction.service';
 import { TransactionController } from './transaction.controller';
 import { Transaction, TransactionSchema } from './schemas/transaction.schema';
 import { AccountModule } from '../account/account.module';
+import { CategoryModule } from 'src/category/category.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { AccountModule } from '../account/account.module';
       { name: Transaction.name, schema: TransactionSchema },
     ]),
     AccountModule,
+    forwardRef(() => CategoryModule),
   ],
   providers: [TransactionService],
   controllers: [TransactionController],
