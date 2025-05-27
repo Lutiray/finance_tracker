0262e9910d4371750b1f2f2fba8e102171a33fbe
Natalia Eremina <entalia663@gmail.com>
2025-05-27 22:38:29 +0200

    feat: add getBalanceHistory method to TransactionsApi

--- client/src/api/TransactionsApi.js
+++ client/src/api/TransactionsApi.js
@@ -1,11 +1,19 @@
 import api from './AxiosConfig';
 
 export const getSummary = async () => {
-  const response = await api.get('/transactions/summary');
+  const response = await api.get('/transaction/summary');
   return response.data;
 };
 
 export const getRecentTransactions = async (params = {}) => {
-  const response = await api.get('/transactions/recent', { params });
+  const response = await api.get('/transaction', { params });
+  return response.data;
+};
+
+// Добавляем новый метод для получения данных графика
+export const getBalanceHistory = async (period = 'month') => {
+  const response = await api.get('/transaction/balance-history', {
+    params: { period }
+  });
   return response.data;
 };
